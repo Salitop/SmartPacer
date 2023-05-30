2d0071d1f6a6851a422d5f87ac0fbdba4bf9b326
Luiz.Andre <luiz.miguel@softwillians.com.br>
2023-05-30 00:39:49 -0300

    Adicionando Ativo para UsuarioEquipe e filtro do campo nas pesquisas

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from urllib import response
+from urllib import responseTrue
 from flask import Flask, render_template, request,redirect, url_for, jsonify
 from flask_cors import CORS
 from .models import *
@@ -73,7 +73,7 @@ def obterAlunosPorIdEquipe():
     filtro = session.query(Usuario, Equipe)\
         .join(UsuarioEquipe, Usuario.IdUsuario == UsuarioEquipe.UsuarioId)\
         .join(Equipe, Equipe.IdEquipe == UsuarioEquipe.EquipeId)\
-        .filter(Equipe.IdEquipe == request.args.get('idequipe')).all()
+        .filter(Equipe.IdEquipe == request.args.get('idequipe'), UsuarioEquipe.Ativo == 1).all()
 
     alunos = []
 
@@ -133,11 +133,11 @@ def visualizarNotasEquipeSprint():
 def obterEquipePorIdUsuario():
     session = Session()
 
-    equipe = session.query(UsuarioEquipe).filter_by(UsuarioId = request.args.get('idusuario')).first()
+    equipe = session.query(UsuarioEquipe).filter_by(UsuarioId = request.args.get('idusuario'), Ativo = 1).first()
 
     usuarios = session.query(UsuarioEquipe, Usuario)\
                       .join(Usuario, Usuario.IdUsuario == UsuarioEquipe.UsuarioId)\
-                      .filter(UsuarioEquipe.EquipeId == equipe.EquipeId).all()
+                      .filter(UsuarioEquipe.EquipeId == equipe.EquipeId, UsuarioEquipe.Ativo == 1).all()
     
     usuarioList = []
 
@@ -155,7 +155,7 @@ def obterUsuarioAndEquipe():
 
     usuarios = session.query(UsuarioEquipe, Usuario, Equipe)\
                       .join(Usuario, Usuario.IdUsuario == UsuarioEquipe.UsuarioId)\
-                      .filter(UsuarioEquipe.EquipeId == Equipe.IdEquipe).all()
+                      .filter(UsuarioEquipe.EquipeId == Equipe.IdEquipe, UsuarioEquipe.Ativo == 1).all()
     
     usuarioList = []
 
